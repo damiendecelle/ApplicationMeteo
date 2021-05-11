aa7904dfc1a42c001f93c6e10b6119c15ee7a6a9
damiendecelle <80416124+damiendecelle@users.noreply.github.com>
2021-05-11 12:08:16 +0200

    Update API_OpenWeather_Air_Quality.js

--- API_OpenWeather_Air_Quality.js
+++ API_OpenWeather_Air_Quality.js
@@ -2,7 +2,7 @@
 function localisation2() {
     var queryLoc = document.getElementById("queryLoc").value;
     // Appel de l'API en fonction de la latitude et la longitude de la ville
-    var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + queryLoc + "&limit=1&appid=932bf7e3763f417ca65743decf57a796"
+    var url = "https://api.openweathermap.org/geo/1.0/direct?q=" + queryLoc + "&limit=1&appid=932bf7e3763f417ca65743decf57a796"
 
     fetch(url)
         // On transforme la réponse de l'API en JSON, c'est plus simple.
@@ -16,7 +16,7 @@ function localisation2() {
         var lon = rep[0].lon;
         console.log("response from lon : ", rep[0].lon);
         // Appel de l'API en fonction de la latitude et la longitude de la ville
-        var url1 = "http://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat + "&lon=" + lon + "&appid=932bf7e3763f417ca65743decf57a796&lang=fr"
+        var url1 = "https://api.openweathermap.org/data/2.5/air_pollution?lat=" + lat + "&lon=" + lon + "&appid=932bf7e3763f417ca65743decf57a796&lang=fr"
 
         fetch(url1)
             // On transforme la réponse de l'API en JSON, c'est plus simple.
